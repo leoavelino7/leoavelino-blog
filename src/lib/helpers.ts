2fcd5881b3c64880b1102882a270f2c0bad2b287
Leonardo Avelino <>
2023-03-11 16:57:34 -0300

    fix(helpers): slugify and sorterBy

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,13 +2,18 @@ import { compareAsc, compareDesc } from "date-fns";
 
 export const isNull = (something: any): something is null => something === null;
 
-export const slugify = (str: string = "") =>
-  str
-    .toLowerCase()
-    .trim()
-    .replace(/[^\w\s-]/g, "")
-    .replace(/[\s_-]+/g, "-")
-    .replace(/^-+|-+$/g, "");
+export const slugify = (text: string = "") => {
+  return text
+    .toString() // Cast to string (optional)
+    .normalize("NFKD") // The normalize() using NFKD method returns the Unicode Normalization Form of a given string.
+    .toLowerCase() // Convert the string to lowercase letters
+    .trim() // Remove whitespace from both sides of a string (optional)
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/[^\w\-]+/g, "") // Remove all non-word chars
+    .replace(/\_/g, "-") // Replace _ with -
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/\-$/g, ""); // Remove trailing -
+};
 
 export const sorterBy = <T extends Object>(list: Array<T>, key: keyof T, mode: "ASC" | "DESC", locale: string) => {
   return list.sort((a, b) => {
@@ -16,8 +21,8 @@ export const sorterBy = <T extends Object>(list: Array<T>, key: keyof T, mode: "
     const valueB = a[key] as any;
 
     const compareByDate = valueA instanceof Date;
-    if(compareByDate){
-      const compareFn = mode === 'ASC' ? compareAsc : compareDesc;
+    if (compareByDate) {
+      const compareFn = mode === "ASC" ? compareAsc : compareDesc;
       return compareFn(new Date(valueA), new Date(valueB));
     }
 
